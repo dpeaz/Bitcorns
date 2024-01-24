3d5465c61eb087ba38b13dced1453802f694fef8
Derek Pease <dcpeaz@gmail.com>
2024-01-23 21:12:56 -0500

    removed req headers in cors middleware

--- server/app.js
+++ server/app.js
@@ -26,16 +26,6 @@ const PORT = process.env.PORT || 4040;
 
 // CORS Middleware
 const cors = (req, res, next) => {
-  req.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type, Accept,Authorization,Origin"
-  );
-  req.setHeader("Access-Control-Allow-Origin", "*");
-  req.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  req.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type, Accept,Authorization,Origin"
